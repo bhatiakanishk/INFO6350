d394d0fda4836067e2083b222034e5a62fe34d36
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-10-06 16:19:08 -0400

    first commit

---
+++ Assignment_3/Assignment_3/Assignment_3/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  Assignment_3
+//
+//  Created by Kanishk Bhatia on 10/6/22.
+//
+import Foundation
+
+//Functions
+//1. Create a function that implements the factorial operator( ! )
