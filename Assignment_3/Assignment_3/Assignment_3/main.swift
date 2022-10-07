4585c5ef40e9f038da14ec53091ee9c34a1941b1
bhatiakanishk <bhatia.ka@northeastern.edu>
2022-10-06 23:34:12 -0400

    First function

--- Assignment_3/Assignment_3/Assignment_3/main.swift
+++ Assignment_3/Assignment_3/Assignment_3/main.swift
@@ -8,4 +8,19 @@ import Foundation
 
 //Functions
 //1. Create a function that implements the factorial operator( ! )
-//2.
+func factorial(n: Int) -> Int {
+    var result = 1
+    if(n>0){
+        for i in 1...n {
+            result *= i
+        }
+    }
+    return result
+}
+let num = 5
+let result = factorial(n: num)
+print("\(num)! = \(result)")
+
+
+//2.Create a function that takes an array of integers as input and split it into two arrays of odd and even integers. Print the result on the console(two arrays).
+//3. Create a function that takes a string as input and checks whether the string is palindrome or not. Print the result on the console (true or false).
