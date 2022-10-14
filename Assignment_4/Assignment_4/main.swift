2aefc80255e18fee7016faf99961e401ffef8a12
bhatiakanishk <bhatia.ka@northeastern.edu>
2022-10-14 17:36:15 -0400

    test

--- Assignment_4/Assignment_4/main.swift
+++ Assignment_4/Assignment_4/main.swift
@@ -2,11 +2,7 @@
 //  main.swift
 //  Assignment_4
 //
-//  Created by Raj Kalpesh Mehta on 10/11/22.
+//  Created by Kanishk Bhatia on 10/13/22.
 //
 
 import Foundation
-
-print("Hello, World!")
-print("mic testing")
-
