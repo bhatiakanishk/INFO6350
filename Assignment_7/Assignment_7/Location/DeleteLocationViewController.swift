459724d36c299216830f277e79614d64f4aed0c7
bhatiakanishk <bhatia.ka@northeastern.edu>
2022-11-06 14:24:32 -0500

    delete location

--- Assignment_7/Assignment_7/Location/DeleteLocationViewController.swift
+++ Assignment_7/Assignment_7/Location/DeleteLocationViewController.swift
@@ -2,13 +2,14 @@
 //  DeleteLocationViewController.swift
 //  Assignment_7
 //
-//  Created by Raj Kalpesh Mehta on 11/6/22.
+//  Created by Kanishk Bhatia on 11/6/22.
 //
 
 import UIKit
 
 class DeleteLocationViewController: UIViewController {
     
+    @IBOutlet weak var tfLocationId: UITextField!
     var mainVC: MainViewController?
     
     override func viewDidLoad() {
@@ -16,16 +17,29 @@ class DeleteLocationViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func btnDeleteTap(_ sender: Any) {
+        guard let mainVC = self.mainVC else { return }
+        
+        let itemId = Int(tfLocationId.text ?? "0") ?? 0
+        if let itemIndex = mainVC.locations.firstIndex(where: {$0.id == itemId}) {
+            mainVC.locations.remove(at: itemIndex)
+            self.showAlert(title: "Success", message: "Deleted successfully")
+        } else {
+            self.showAlert(title: "Error", message: "Invalid Location Id")
+        }
+        
     }
-    */
-
+    
+    @IBAction func btnCloseTap(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    func showAlert(title: String, message: String) {
+        let alertView = UIAlertView(title: title,
+                                    message: message,
+                                    delegate: self,
+                                    cancelButtonTitle: "Okay")
+        alertView.show()
+    }
 }
