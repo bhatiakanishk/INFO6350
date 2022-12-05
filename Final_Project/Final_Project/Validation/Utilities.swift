ca5a1feb63a6d523fb51f6e1a6bc6c5a80322201
bhatiakanishk <bhatia.ka@northeastern.edu>
2022-12-05 15:39:43 -0500

    date formatter

--- Final_Project/Final_Project/Validation/Utilities.swift
+++ Final_Project/Final_Project/Validation/Utilities.swift
@@ -9,6 +9,8 @@ import Foundation
 import UIKit
 
 class Utilities {
+    
+    // Check Email
     static func emailValid(_ emailTf : String) -> Bool {
         let emailRegex = "(?:[a-z0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[a-z0-9!#$%\\&'*+/=?\\^_`{|}"+"~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\"+"x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-"+"z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5"+"]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-"+"9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21"+"-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
         
@@ -16,11 +18,25 @@ class Utilities {
         return testEmail.evaluate(with: emailTf)
     }
     
+    // Check Password
     static func passwordValid(_ password : String) -> Bool {
         let passwordRegex =
         "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}"
         
         let passwordTest = NSPredicate(format: "SELF MATCHES %@", passwordRegex)
         return passwordTest.evaluate(with: password)
+    }
+    
+    // Clean up text
+    static func textInput(_ input: String) -> String {
+        return input.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    
+    // Date Formatter
+    static func dateFormatter(_ date: Date) -> String {
+        let printDateFormatter = DateFormatter()
+        printDateFormatter.dateFormat = "MMM dd, yyyy h:mm a"
+        
+        return printDateFormatter.string(from: date)
     }
 }
