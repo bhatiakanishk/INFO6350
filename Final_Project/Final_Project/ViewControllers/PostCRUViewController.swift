9067935c8db362e470771a19fd61ae364376d13d
bhatiakanishk <bhatia.ka@northeastern.edu>
2022-12-14 15:44:35 -0500

    PostCRUViewController

--- Final_Project/Final_Project/ViewControllers/PostCRUViewController.swift
+++ Final_Project/Final_Project/ViewControllers/PostCRUViewController.swift
@@ -24,11 +24,108 @@ class PostCRUViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
+    @objc func tapDone(sender: Any) {
+        self.view.endEditing(true)
+    }
+    
+    func clearError() {
+        errorLabel.text = "error"
+        errorLabel.alpha = 0
+    }
 
+    func showError(_ message:String) {
+        errorLabel.text = message
+        errorLabel.alpha = 1
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        let nextTag = textField.tag + 1
+
+        if let nextResponder = textField.superview?.viewWithTag(nextTag) {
+            nextResponder.becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+        }
+
+        return true
+    }
+    
+    func validateFields() -> String? {
+        if Utilities.textInput(titleLabel.text!) == "" ||
+            Utilities.textInput(descriptionTf.text!) == "" {
+            return "Please enter all details."
+        }
+        return nil
+    }
+    
+    @IBAction func doAction(_ sender: UIButton) {
+        let error = validateFields()
+        
+        if error != nil {
+            self.showError(error!)
+        } else {
+            let title = Utilities.textInput(titleTf.text!)
+            let body = Utilities.textInput(descriptionTf.text!)
+            
+            var alertMessage: String = ""
+            
+            if post != nil {
+                db.collection("posts").document((post?.id)!).updateData([
+                    "title": title,
+                    "body": body,
+                    "lastUpdated": FieldValue.serverTimestamp()
+                ]) { err in
+                    if let err = err {
+                        self.showError("error saving data")
+                    }
+                }
+                
+                alertMessage = "Post updated successfully"
+            } else {
+                db.collection("users").whereField("uid", isEqualTo: user?.uid).getDocuments() {(querySnapshot, err) in
+                    if let error = err {
+                        print(error)
+                        return
+                    }
+                    
+                    guard let documents = querySnapshot?.documents else {
+                        self.showError("error saving data")
+                        return
+                    }
+                    
+                    let dbUser = documents.compactMap { queryDocumentSnapshot -> User? in
+                        return try? queryDocumentSnapshot.data(as: User.self)
+                    }[0]
+                    
+                    let newPost = Post(title: title, body: body, createdById: (self.user?.uid)!, createdByName: "\(dbUser.firstName) \(dbUser.lastName)") //Check for timr
+                    
+                    do {
+                        let _ = try self.db.collection("posts").addDocument(from: newPost)
+                    } catch {
+                        self.showError("error creating post")
+                    }
+                }
+                
+                alertMessage = "Post created successfully"
+            }
+            
+            let alert = UIAlertController(title: "Message", message: alertMessage, preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "Ok", style: .cancel) { _ in
+                self.navigationController?.popViewController(animated: true)
+            })
+            
+            present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
